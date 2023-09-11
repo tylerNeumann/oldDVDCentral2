ad90f92df5e2deddf7047d8c485f62ebd17d6b0c
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2023-09-11 18:55:24 -0500

    need image paths cust & mov movie descriptions fix order items

--- TN.DVDCentral.DB/PostDeploy/DefaulfData/OrderItems.sql
+++ TN.DVDCentral.DB/PostDeploy/DefaulfData/OrderItems.sql
@@ -1,7 +1,7 @@
 ﻿BEGIN
-	INSERT INTO tblOrderItem (Id,OrderId,Quantity,MovieId,Cost)
+	INSERT INTO tblOrderItem (Id,Quantity,MovieId,Cost)
 	VALUES
-	(1,),
-	(2,),
-	(3,)
+	(1, 1, 1, 3.25),
+	(2, 1, 2, 4.50),
+	(3, 1, 3, 2.75)
 END
