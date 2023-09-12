051cb8ecda9c277e08b6b53ee948babe1a505b1e
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2023-09-11 22:12:55 -0500

    added orderItem props

--- TN.DVDCentral.BL.Models/OrderItem.cs
+++ TN.DVDCentral.BL.Models/OrderItem.cs
@@ -8,5 +8,10 @@ namespace BL.Models
 {
     public class OrderItem
     {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int Quantity { get; set; }
+        public int MovieId { get; set; }
+        public float Cost { get; set; }
     }
 }
