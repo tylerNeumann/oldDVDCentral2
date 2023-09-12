d41b80aec7f25ee4102f05b9564ca65213a72bbe
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2023-09-11 22:09:50 -0500

    added movie props

--- TN.DVDCentral.BL.Models/Movie.cs
+++ TN.DVDCentral.BL.Models/Movie.cs
@@ -9,5 +9,12 @@ namespace BL.Models
     public class Movie
     {
         public int Id { get; set; }
+        public string? Descriptioj { get; set; }
+        public int FormatId { get; set; }
+        public int DirectorId { get; set; }
+        public int RatingId { get; set; }
+        public float Cost { get; set; }
+        public int Quantity { get; set; }
+        public string? ImagePath { get; set; }
     }
 }
