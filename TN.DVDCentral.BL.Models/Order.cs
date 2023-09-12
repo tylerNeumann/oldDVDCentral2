1e7fd3cff0bf89a4cbee3e18c4ffecdd96083152
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2023-09-11 22:11:21 -0500

    added order props

--- TN.DVDCentral.BL.Models/Order.cs
+++ TN.DVDCentral.BL.Models/Order.cs
@@ -8,5 +8,9 @@ namespace BL.Models
 {
     public class Order
     {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int UserId { get; set; }
     }
 }
