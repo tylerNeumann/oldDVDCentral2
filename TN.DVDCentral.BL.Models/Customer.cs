7881f440c9fb6d0d58e3d5810217de1c329137d9
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2023-09-11 22:06:55 -0500

    added customer properties

--- TN.DVDCentral.BL.Models/Customer.cs
+++ TN.DVDCentral.BL.Models/Customer.cs
@@ -8,5 +8,14 @@ namespace BL.Models
 {
     public class Customer
     {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int UserId { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? ZIP { get; set; }
+        public string? Phone { get; set; }
+        public string? ImagePath { get; set; }  
     }
 }
