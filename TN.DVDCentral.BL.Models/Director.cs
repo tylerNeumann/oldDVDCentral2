cd780e087b2ead7112e31d1f1e8c77c3f79da97c
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2023-09-11 19:06:02 -0500

    finished director's properties

--- TN.DVDCentral.BL.Models/Director.cs
+++ TN.DVDCentral.BL.Models/Director.cs
@@ -8,5 +8,8 @@ namespace BL.Models
 {
     public class Director
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
