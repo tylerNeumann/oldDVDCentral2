2a856a70c9072ba6d62b4f73449652d950d436f8
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2023-09-13 11:20:58 -0500

    moved movie images top movie folder added image path for movies

--- TN.DVDCentral.DB/PostDeploy/DefaultData/Movies.sql
+++ TN.DVDCentral.DB/PostDeploy/DefaultData/Movies.sql
@@ -1,7 +1,7 @@
 ﻿BEGIN
 	INSERT INTO tblMovie(Id,Title,Description,FormatId,DirectorId,RatingId,Cost,Quantity,ImagePath)
 	VALUES
-	(1, 'Fast & Furious', 'After six years on the lam, fugitive Dom Toretto returns to Los Angeles and teams up with rogue FBI agen Brian O''Conner to bring down a common enemy.', 1,3,3, 3.25,3,''),
-	(2, 'Star Wars Revenge of the Sith', 'Three years into the Clone Wars, Obi-Wan pursues a new threat, while Anakin is lured by Chancellor Palpatine into a sinister plot to rule the galaxy.', 2, 2, 3, 4.50, 5, ''),
-	(3, 'Jaws', 'When an insatiable great white shark terrorizes Amity Island, a police chief, an oceanographer and a grizzled shark hunter seek to destroy the beast', 3, 1, 2, 2.75, 2, '')
+	(1, 'Fast & Furious', 'After six years on the lam, fugitive Dom Toretto returns to Los Angeles and teams up with rogue FBI agen Brian O''Conner to bring down a common enemy.', 1,3,3, 3.25,3,'TN.DVDCentral\img\movies\fast_and_furious.jpeg'),
+	(2, 'Star Wars Revenge of the Sith', 'Three years into the Clone Wars, Obi-Wan pursues a new threat, while Anakin is lured by Chancellor Palpatine into a sinister plot to rule the galaxy.', 2, 2, 3, 4.50, 5, 'TN.DVDCentral\img\movies\Revenge_of_the_Sith.jpg'),
+	(3, 'Jaws', 'When an insatiable great white shark terrorizes Amity Island, a police chief, an oceanographer and a grizzled shark hunter seek to destroy the beast', 3, 1, 2, 2.75, 2, 'TN.DVDCentral\img\movies\jaws.jpeg')
 END
