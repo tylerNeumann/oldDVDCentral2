5b5751067a45f7377d3662976ab4b6c0e8e89929
Tyler Neumann <500189307@fvtc.edu>
2023-09-21 12:36:13 -0500

    fixed tblCustomer

--- TN.DVDCentral.DB/Tables/tblCustomer.sql
+++ TN.DVDCentral.DB/Tables/tblCustomer.sql
@@ -6,7 +6,7 @@
 	[UserId] INT NOT NULL,
 	[Address] VARCHAR(50) NOT NULL,
 	[City] VARCHAR(25) NOT NULL,
+	[State] VARCHAR(2),
 	[ZIP] VARCHAR(12) NOT NULL,
 	[Phone] VARCHAR(20) NOT NULL,
-	[ImagePath] VARCHAR(100) NOT NULL
 )
