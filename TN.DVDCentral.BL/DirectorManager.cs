c70807e0bd8331576487f18d0d97333f5e687e4b
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2023-09-28 22:40:17 -0500

    added director manager

--- TN.DVDCentral.BL/DirectorManager.cs
+++ TN.DVDCentral.BL/DirectorManager.cs
@@ -1,54 +1,148 @@
 ﻿using BL.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Data;
+using System.Transactions;
+using TN.DVDCentral.PL;
 
 namespace TN.DVDCentral.BL
 {
     public static class DirectorManager
     {
-        public static int Insert(Director director, bool rollback = false) { return 0; }
-        public static int Update()
+        public static int Insert(Director director, bool rollback = false)
         {
             try
             {
-                return 0;
+                int result = 0;
+                using(DVDCentralEntities dc = new DVDCentralEntities())
+                {
+                    IDbContextTransaction transaction = null;
+                    if (rollback) transaction = dc.Database.BeginTransaction();
+                    tblDirector entity = new tblDirector();
+                    entity.Id = dc.tblDirectors.Any() ? dc.tblDirectors.Max(s => s.Id) + 1 : 1;
+                    entity.FirstName = director.FirstName;
+                    entity.LastName = director.LastName;
+                    entity.Id = director.Id;
+                    dc.Add(entity);
+                    result = dc.SaveChanges();
+                    if(rollback) transaction.Rollback();
+                }
+                return result;
             }
             catch (Exception)
             {
 
                 throw;
             }
+           
         }
-        public static int Delete()
+        public static int Update(Director director, bool rollback = false)
         {
             try
             {
-                return 0;
+                int result = 0;
+                using (DVDCentralEntities dc = new DVDCentralEntities())
+                {
+                    IDbContextTransaction transaction = null;
+                    if (rollback) transaction = dc.Database.BeginTransaction();
+                    tblDirector entity = dc.tblDirectors.FirstOrDefault(s => s.Id == director.Id);
+                    if(entity != null)
+                    {
+                        entity.FirstName = director.FirstName;
+                        entity.LastName = director.LastName;
+                        entity.Id = director.Id;
+                        result = dc.SaveChanges();
+                    }
+                    else
+                    {
+                        throw new Exception("row doesn't exist");
+                    }
+                    if (rollback) transaction.Rollback();
+                }
+                return result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+        public static int Delete(int id, bool rollback = false)
+        {
+            try
+            {
+                int result = 0;
+                using(DVDCentralEntities dc = new DVDCentralEntities())
+                {  
+                    IDbContextTransaction transaction = null;
+                    if(rollback) transaction = dc.Database.BeginTransaction();
+                    tblDirector entity = dc.tblDirectors.FirstOrDefault(s => s.Id == id);
+                    if (entity != null)
+                    {
+                        dc.Remove(entity);
+                        result = dc.SaveChanges();
+                    }
+                    else { throw new Exception("row doesn't exist"); }
+                    if (rollback) transaction.Rollback();
+                }
+                return result;
             }
             catch (Exception)
             {
 
                 throw;
             } }
-        public static Director LoadById()
+        public static Director LoadById(int id)
         {
             try
             {
-                return null;
+                using(DVDCentralEntities dc = new DVDCentralEntities())
+                {  
+                    tblDirector entity = dc.tblDirectors.FirstOrDefault(director => director.Id == id);
+                    if(entity != null)
+                    {
+                        return new Director()
+                        {
+                            Id = entity.Id,
+                            FirstName = entity.FirstName,
+                            LastName = entity.LastName
+                        };
+                    }
+                    else {
+
+                        throw new Exception();
+                    }
+                }
             }
             catch (Exception)
             {
 
                 throw;
-            } }
+            } 
+        }
         public static List<Director> Load() 
         {
             try
             {
-                return null;
+                List<Director> list = new List<Director>();
+                using(DVDCentralEntities dc = new DVDCentralEntities())
+                {
+                    (from d in dc.tblDirectors
+                     select new
+                     {
+                         d.Id,
+                         d.FirstName,
+                         d.LastName
+                     })
+                     .ToList()
+                     .ForEach(director => list.Add(new Director
+                     {
+                         Id = director.Id,
+                         FirstName = director.FirstName,
+                         LastName = director.LastName
+                     }));
+                }
+                return list;
             }
             catch (Exception)
             {
