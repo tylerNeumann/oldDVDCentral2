3f0d717e8ea09ea0e5eb8409f3d33b08af425d78
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2023-11-10 21:48:32 -0600

    working on sidebar

---
+++ TN.DVDCentral.UI/ViewComponents/SideBar.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TN.DVDCentral.UI.ViewComponents
+{
+    public class SideBar : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(GenreManager.Load().OrderBy(g =>g.Description));
+        }
+    }
+}
