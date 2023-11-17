ec1e69924a8af96467dcad40773c4d3dae74bab0
tylerNeumann <124943406+tylerNeumann@users.noreply.github.com>
2023-11-17 17:15:54 -0600

    added authentication class

---
+++ TN.DVDCentral.UI/Models/Authentication.cs
@@ -0,0 +1,17 @@
+﻿namespace TN.DVDCentral.UI.Models
+{
+    public class Authentication
+    {
+        public static bool IsAuthenticated(HttpContext context)
+        {
+            if (context.Session.GetObject<User> != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
