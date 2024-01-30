bbd84dbaf9866392e870e99b5ada58bfdbe64b76
Tyler Neumann <500189307@fvtc.edu>
2024-01-30 14:19:07 -0600

    added bl generic manager

---
+++ TN.DVDCentral.BL/GenericManager.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using TN.DVDCentral.PL2.Data;
+
+namespace TN.DVDCentral.BL
+{
+    public abstract class GenericManager<T> where T : class
+    {
+        protected DbContextOptions<DVDCentralEntities> options;
+
+        public GenericManager(DbContextOptions<DVDCentralEntities> options)
+        {
+            this.options = options;
+        }
+
+        public List<T> Load() 
+        {
+            return null;
+        }
+        public List<T> LoadById(Guid id)
+        {
+            return null;
+        }
+
+        public int Insert(T entity, bool rollback = false)
+        {
+            return 0;
+        }
+
+        public int Update(T entity, bool rollback = false)
+        {
+            return 0;
+        }
+
+        public int Delete(Guid id, bool rollback = false)
+        {
+            return 0;
+        }
+    }
+}
