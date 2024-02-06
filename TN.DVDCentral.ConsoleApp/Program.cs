e10d521d5a3e97cb1e70a81daafa6e4e3bc19555
Tyler Neumann <500189307@fvtc.edu>
2024-02-06 14:17:59 -0600

    added signalR connection

---
+++ TN.DVDCentral.ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using TN.DVDCentral.ConsoleApp;
+
+internal class Program
+{
+    private static string DrawMenu()
+    {
+        Console.WriteLine("Which operation do you wish to perform?");
+        Console.WriteLine("connect to a channel (c)");
+        Console.WriteLine("send a message to the channel (s)");
+        Console.WriteLine("Exit (x)");
+        string operation = Console.ReadLine();
+        return operation;
+    }
+    private static void Main(string[] args)
+    {
+        string user = "Neumann";
+        string hubAddress = "https://fvtcdp.azurewebsites.net/GameHub";
+        string operation = DrawMenu();
+
+        var signalRConnection = new SignalRConnection(hubAddress);
+
+        while(operation != "x") 
+        {
+            switch (operation)
+            {
+                case "c":
+                    signalRConnection.ConnectToChannel(user);
+                    break;
+                case "s":
+                    break;
+                case "x":
+                    break; 
+            }
+
+            operation = DrawMenu();
+        }
+    }
+}
