125f805cb49e88ca3132e4f9b68c4688277328b9
Tyler Neumann <500189307@fvtc.edu>
2024-02-15 13:57:32 -0600

    fixed utbase namespace errors

---
+++ TN.DVDCentral.API.Test/GlobalUsings.cs
@@ -0,0 +1,9 @@
+global using Microsoft.AspNetCore.Mvc.Testing;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using Newtonsoft.Json;
+global using Newtonsoft.Json.Linq;
+global using System;
+global using System.Collections.Generic;
+global using System.Reflection;
+global using System.Threading.Tasks;
