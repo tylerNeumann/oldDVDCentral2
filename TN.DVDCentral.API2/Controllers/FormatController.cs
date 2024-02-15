97f79873923b29b364d4750dcc8e446f40cc6397
Tyler Neumann <500189307@fvtc.edu>
2024-02-15 12:49:00 -0600

    added new api proj

---
+++ TN.DVDCentral.API2/Controllers/FormatController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TN.DVDCentral.BL;
+using TN.DVDCentral.BL.Models;
+using TN.DVDCentral.PL2.Data;
+
+
+namespace TN.DVDCentral.API2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormatController : ControllerBase
+    {
+        private readonly DbContextOptions<DVDCentralEntities> options;
+        public FormatController(DbContextOptions<DVDCentralEntities> options)
+        {
+            this.options = options;
+        }
+
+        [HttpGet]
+        public IEnumerable<Format> Get()
+        {
+            return new FormatManager(options).Load();
+        }
+    }
+}
