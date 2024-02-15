705e2d1eb097e89aecfe3c0a288ef42a7a25486d
Tyler Neumann <500189307@fvtc.edu>
2024-02-15 13:31:20 -0600

    added movie controller to api

---
+++ TN.DVDCentral.API2/Controllers/MovieController.cs
@@ -0,0 +1,71 @@
+﻿namespace TN.DVDCentral.API2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly ILogger<MovieController> logger;
+        private readonly DbContextOptions<DVDCentralEntities> options;
+        public MovieController(ILogger<MovieController> logger,
+                                DbContextOptions<DVDCentralEntities> options)
+        {
+            this.options = options;
+            this.logger = logger;
+            logger.LogWarning("I was here!");
+        }
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return new MovieManager(options).Load();
+        }
+
+        [HttpGet]
+        public Movie Get(Guid id)
+        {
+            return new MovieManager(options).LoadById(id);
+        }
+
+        [HttpPost("{rollback?}")]
+        public int Post([FromBody] Movie movie, bool rollback = false)
+        {
+            try
+            {
+                return new MovieManager(options).Insert(movie, rollback);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        [HttpPut("{id}/{rollback?}")]
+        public int Put(Guid id, [FromBody] Movie movie, bool rollback = false)
+        {
+            try
+            {
+                return new MovieManager(options).Update(movie, rollback);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        [HttpDelete("{id}/{rollback?}")]
+        public int Delete(Guid id, bool rollback = false)
+        {
+            try
+            {
+                return new MovieManager(options).Delete(id, rollback);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
