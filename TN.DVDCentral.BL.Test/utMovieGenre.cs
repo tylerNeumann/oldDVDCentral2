37ecb5d790fceaf279eb401ecc2f88024e86ee44
Tyler Neumann <500189307@fvtc.edu>
2024-02-16 21:23:23 -0600

    moviegenre done

--- TN.DVDCentral.BL.Test/utMovieGenre.cs
+++ TN.DVDCentral.BL.Test/utMovieGenre.cs
@@ -1,39 +1,31 @@
-﻿
-using static TN.DVDCentral.BL.MovieGenreManager;
-
-namespace TN.DVDCentral.BL.Test
+﻿namespace TN.DVDCentral.BL.Test
 {
     [TestClass]
-    public class utMovieGenre
+    public class utMovieGenre : utBase
     {
 
         [TestMethod]
         public void InsertTest()
         {
-            int id = 0;
-            MovieGenre movieGenre = new MovieGenre
-            {
-                MovieId = 99,
-                GenreId = 99
-            };
-            //int results = MovieGenreManager.Insert(movieGenre, true);
-            //Assert.AreEqual(1, results);
+            Guid movieId = new MovieManager(options).Load().FirstOrDefault().Id;
+            Guid genreId = new GenreManager(options).Load().FirstOrDefault().Id;
+            int results = new MovieGenreManager(options).Insert(movieId, genreId, true);
+            Assert.IsTrue(results > 0);
         }
+        //[TestMethod]
+        //public void UpdateTest()
+        //{
+        //    int id = 0;
+        //    MovieGenre movieGenre = MovieGenreManager.LoadById(2);
+        //    movieGenre.GenreId = 9999;
+        //    int results = MovieGenreManager.Update(movieGenre, true);
+        //    Assert.AreEqual(1, results);
+        //}
         [TestMethod]
-        public void UpdateTest()
+        public void DeleteTest()
         {
-            int id = 0;
-            MovieGenre movieGenre = MovieGenreManager.LoadById(2);
-            movieGenre.GenreId = 9999;
-            int results = MovieGenreManager.Update(movieGenre, true);
-            Assert.AreEqual(1, results);
-        }
-        [TestMethod]
-        public void DeleteTest()S
-        {
-            int id = 0;
-            int results = MovieGenreManager.Delete(3, true);
-            Assert.AreEqual(1, results);
+            tblMovieGenre row = new MovieGenreManager(options).Load().FirstOrDefault();
+            Assert.IsTrue(new MovieGenreManager(options).Delete(row.MovieId, row.GenreId, true) > 0);
         }
     }
 }
